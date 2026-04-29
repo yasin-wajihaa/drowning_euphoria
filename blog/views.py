9528731a06d54ffb9cfd57f8d6fb941125608251
yasin-wajihaa <wajihaazafar@gmail.com>
2026-04-29 13:30:28 +0530

    added templates folder and rendered home and about html

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def Home(request):
+    return render(request, 'blog/home.html')
+
+def About(request):
+    return render(request, 'blog/about.html')
