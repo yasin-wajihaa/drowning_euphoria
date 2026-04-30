2623d0d88aa1b7c46679381316be84f6b6b08414
yasin-wajihaa <wajihaazafar@gmail.com>
2026-04-30 22:41:19 +0530

    added database and deleted dummy data and made admin site access posts

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Post
 
 posts = [
     {
@@ -17,7 +17,7 @@ posts = [
 ]
 def Home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
